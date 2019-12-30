00cb78926250fb53ed582beb5e8febc1857b9f6b
przemo06133 <przemo06133@gmail.com>
2019-12-30 20:51:39 +0100

    Initial commit

---
+++ src/main/java/com/example/springsecurityyoutubeamigoscode/SpringSecurityYoutubeAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityyoutubeamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityYoutubeAmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityYoutubeAmigosCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityyoutubeamigoscode/SpringSecurityYoutubeAmigosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityyoutubeamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityYoutubeAmigosCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
